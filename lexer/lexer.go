94da09f8408bb72fec54f9af0b437d79de0f8b49
Sauyon Lee <git@sauyon.com>
2014-01-16 01:32:40 -0800

    rework lexer to deal with open/close tags and add some unit tests for the lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,27 +7,44 @@ import (
 	"regexp"
 )
 
+type Flags int
+
+const (
+	NoParseInner Flags = 1 << iota
+	BodyAsArg
+	NoNewline
+	RequireClose
+)
+
+type Expression struct {
+	Expr      string
+	CloseExpr string
+
+	Flags Flags
+}
+
 // A Matcher pairs a set of regexps and a set of tokens.
 type Matcher interface {
-	Name() string // A unique name for this matcher
-	Expr() string // The regular expressions that match this token
+	Name() string        // A unique name for this matcher
+	Exprs() []Expression // The regular expressions that match this token
 
-	IsValid(args *token.TokenArgs) bool
-	BuildToken(args *token.TokenArgs) token.Token
+	IsValid(args *token.TokenArgs, expNum int) bool
+	BuildToken(args *token.TokenArgs, expNum int) (openToken token.Token, closeToken token.Token)
 }
 
 /*
  * This is an implementation of the a Lexer, used to convert text into tokens
  * (http://en.wikipedia.org/wiki/Lexical_analysis) using the regexp package.
-
+ *
  * Because the regexp package is implemented using a NFA
  * (http://en.wikipedia.org/wiki/Nondeterministic_finite_automaton),
  * it's very effective for this use case.
  */
 type Lexer struct {
-	matchers  map[string]Matcher
-	regexps   map[string]*regexp.Regexp
-	subexpIds map[string]int
+	matchers   map[string]Matcher
+	regexps    map[string][]*regexp.Regexp
+	subexpIds  map[string][]int
+	bodyExpIds map[string][]int
 
 	expr   string         // The main regexp expression
 	regexp *regexp.Regexp // The regexp used to match tags
@@ -43,17 +60,56 @@ func Must(l *Lexer, err error) *Lexer {
 func New(matchers ...Matcher) (*Lexer, error) {
 	var err error
 	l := &Lexer{
-		matchers:  make(map[string]Matcher),
-		regexps:   make(map[string]*regexp.Regexp),
-		subexpIds: make(map[string]int),
+		matchers:   make(map[string]Matcher),
+		regexps:    make(map[string][]*regexp.Regexp),
+		subexpIds:  make(map[string][]int),
+		bodyExpIds: make(map[string][]int),
 	}
 
 	for _, matcher := range matchers {
 		l.matchers[matcher.Name()] = matcher
-		l.expr += fmt.Sprintf("(?P<_%s>%s)|", matcher.Name(), matcher.Expr())
-		l.regexps[matcher.Name()], err = regexp.Compile(matcher.Expr())
-		if err != nil {
-			return nil, err
+
+		numExprs := len(matcher.Exprs())
+		l.regexps[matcher.Name()] = make([]*regexp.Regexp, numExprs, numExprs)
+		l.subexpIds[matcher.Name()] = make([]int, numExprs, numExprs)
+		l.bodyExpIds[matcher.Name()] = make([]int, numExprs, numExprs)
+		for i, expr := range matcher.Exprs() {
+			var argExpr, realExpr string
+			if expr.CloseExpr != "" {
+				closeExpr := expr.CloseExpr
+				if expr.Flags&RequireClose == 0 {
+					closeExpr = "(?:" + expr.CloseExpr + ")|$"
+				}
+
+				var bodyExpr, rBodyExpr string
+				if expr.Flags&BodyAsArg != 0 {
+					bodyExpr += "?:"
+				}
+				if expr.Flags&NoNewline == 0 {
+					bodyExpr += "(?s)"
+					rBodyExpr += "(?s)"
+				}
+
+				argExpr = fmt.Sprintf("(?:%s)(%s.*?)(?:%s)", expr.Expr, bodyExpr, closeExpr)
+				realExpr = fmt.Sprintf("(?:%s)(?P<_i%02x%s>%s.*?)(?:%s)",
+					expr.Expr, i, matcher.Name(), rBodyExpr, closeExpr)
+			} else {
+				argExpr = "(?:" + expr.Expr + ")(?-imsU)"
+				realExpr = "(?:" + expr.Expr + ")(?-imsU)"
+			}
+
+			l.regexps[matcher.Name()][i], err = regexp.Compile(argExpr)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, subexpName := range l.regexps[matcher.Name()][i].SubexpNames() {
+				if subexpName != "" && subexpName[0] == '_' {
+					return nil, errors.New("lexer: capture group names starting with _ are reserved for use by the lexer! Your name is " + subexpName)
+				}
+			}
+
+			l.expr += fmt.Sprintf("(?P<_%02x%s>%s)|", i, matcher.Name(), realExpr)
 		}
 	}
 
@@ -65,13 +121,24 @@ func New(matchers ...Matcher) (*Lexer, error) {
 
 	for i, name := range l.regexp.SubexpNames() {
 		if name != "" && name[0] == '_' {
-			name = name[1:] // Remove the '_' prefix
-			_, ok := l.matchers[name]
-			if !ok {
-				return nil, errors.New("lexer: capture group names starting with _ are reserved for use by the lexer! Your name is " + name)
-			}
+			var expNum int
+			var matcherName string
+			var err error
+			if name[1] == 'i' {
+				_, err = fmt.Sscanf(name, "_i%02x%s", &expNum, &matcherName)
+				if err != nil {
+					return nil, errors.New("Something went wrong while compiling D:")
+				}
 
-			l.subexpIds[name] = i
+				l.bodyExpIds[matcherName][expNum] = i
+			} else {
+				_, err = fmt.Sscanf(name, "_%02x%s", &expNum, &matcherName)
+				if err != nil {
+					return nil, errors.New("Something went wrong while compiling D:")
+				}
+
+				l.subexpIds[matcherName][expNum] = i
+			}
 		}
 	}
 
@@ -83,31 +150,57 @@ func New(matchers ...Matcher) (*Lexer, error) {
  */
 func (l *Lexer) Tokenize(data string) []token.Token {
 	ret := make([]token.Token, 0)
+	toAppend := ""
 
 	for data != "" {
 		indices := l.regexp.FindStringSubmatchIndex(data)
 		if indices == nil {
-			ret = append(ret, token.TextToken{data})
+			ret = append(ret, token.TextToken{toAppend + data})
 			break
 		}
 
 		for name, matcher := range l.matchers {
-			i := l.subexpIds[name]
-			if indices[i*2] >= 0 {
-				if indices[i*2] != 0 {
-					ret = append(ret, token.TextToken{data[:indices[i*2]]})
+			for expNum, i := range l.subexpIds[name] {
+				if i != 0 && indices[i*2] >= 0 {
+					if indices[i*2] != 0 {
+						toAppend += data[:indices[i*2]]
+					}
+
+					args := l.regexps[name][expNum].FindStringSubmatch(data[indices[0]:indices[1]])
+
+					tokenArgs := token.NewTokenArgs(args, l.regexps[name][expNum].SubexpNames())
+
+					if matcher.IsValid(tokenArgs, expNum) {
+						openToken, closeToken := matcher.BuildToken(tokenArgs, expNum)
+
+						if openToken != nil {
+							if toAppend != "" {
+								ret = append(ret, token.TextToken{toAppend})
+							}
+							ret = append(ret, openToken)
+						}
+
+						bodyExpId := l.bodyExpIds[name][expNum]
+						if matcher.Exprs()[expNum].Flags&(NoParseInner|BodyAsArg) == 0 && bodyExpId != 0 {
+							ret = append(ret, l.Tokenize(data[indices[bodyExpId*2]:indices[bodyExpId*2+1]])...)
+						} else {
+							toAppend += data[indices[bodyExpId*2]:indices[bodyExpId*2+1]]
+						}
+
+						if closeToken != nil {
+							if toAppend != "" {
+								ret = append(ret, token.TextToken{toAppend})
+								toAppend = ""
+							}
+							ret = append(ret, closeToken)
+						}
+
+						data = data[indices[i*2+1]:]
+					} else {
+						toAppend += data[:1]
+						data = data[1:]
+					}
 				}
-
-				args := []string(l.regexps[name].FindStringSubmatch(data[indices[0]:indices[1]]))
-
-				tokenArgs := token.NewTokenArgs(args, l.regexps[name].SubexpNames())
-
-				if matcher.IsValid(tokenArgs) {
-					ret = append(ret, matcher.BuildToken(tokenArgs))
-				} else {
-					ret = append(ret, token.TextToken{data[indices[i*2]:indices[i*2+1]]})
-				}
-				data = data[indices[i*2+1]:]
 			}
 		}
 	}
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -3,71 +3,73 @@ package lexer_test
 import (
 	"."
 	"fmt"
+	"github.com/moechat/parser/token"
+	"testing"
 )
 
-func ExampleLexer_AddTokenClass() {
-	// The ID's are strings because I'm too lazy to use strconv (and not lazy enough to document the reason, apparently)
-	users := map[string]string{"alice": "0", "bob": "1"}
+type TestMatcher struct {
+	name  string
+	exprs []lexer.Expression
+}
 
-	matcher := parser.NewTokenClass(
-		parser.TokenClassArgs{
-			ArgModFunc: func(args []string, idByName map[string]int) ([]string, map[string]int) {
-				uid := users[args[idByName["username"]]]
-				uidIndex := len(args)
-				args = append(args, uid)
-				idByName["uid"] = uidIndex
-				return args, idByName
-			},
-			IsValid: func(args *token.TokenArgs) bool {
-				return args.ByName("uid") != ""
-			},
+func (tm *TestMatcher) Name() string {
+	return tm.name
+}
 
-			Tokens: []token.Token{
-				&htmltoken.Token{
-					Name: "span",
-					Type: token.SingleType, // This is the default
+func (tm *TestMatcher) Exprs() []lexer.Expression {
+	return tm.exprs
+}
 
-					Prefix:  "{{.ById 1}}",
-					Classes: []string{"at-tag", "{{.ByName uid}}"},
-					Attributes: map[string]string{
-						"data-user": "{{.ById 2}}",
-						"data-uid":  "{{.ByName uid}}",
-					},
-					// CssProps is not necessary here, but behaves in the same way as Attributes
-
-					// This is overkill in this case, and here only as an example. Note that OutputFunc, if not nil, makes MoeParser ignore all other options.
-					OutputFunc: func(args *token.TokenArgs) string {
-						output := args.ById(1)
-						output += `<span class="at-tag user-` + args.ByName("uid") + `"`
-						output += `data-uid="` + args.ByName("uid") + `" `
-						output += `data-user="` + args.ById(2) + `">`
-						output += args.ById(2)
-						output += `</span>`
-						return output
-					},
-				},
-			},
-		},
-		`(^|\s)@(?P<username>\S+)`)
-
-	l, _ := lexer.New(make(map[string]token.TokenClass))
-
-	l.AddTokenClass(matcher)
-
-	l.CompileRegexp() // This is necessary for Tokenize to take into account changes due to AddMoeMatcher or RemoveMoeMatcher
-
-	testStr :=
-		`@alice and @bob say that @charlie sucks because he doesn't use {{insert awesome service here}}`
-
-	for _, token := range l.Tokenize(testStr) {
-		t, err := token.Output()
-		if err != nil {
-			panic(err)
+func (tm *TestMatcher) IsValid(args *token.TokenArgs, expNum int) bool {
+	if tm.name == "image" {
+		if args.ById(1) == "" {
+			return false
 		}
-		fmt.Print(t)
+	}
+	return true
+}
+
+func (tm *TestMatcher) BuildToken(args *token.TokenArgs, expNum int) (token.Token, token.Token) {
+	if tm.name == "image" {
+		url := args.ById(1)
+		title := url
+		if expNum == 2 {
+			title = args.ById(2)
+		}
+		// Yes, this would be unsafe in a production environment. But it's a testing script.
+		return token.TextToken{fmt.Sprintf(`<img src="%s" title="%s">`, url, title)}, nil
+	} else if tm.name == "bold" {
+		return token.TextToken{"<b>"}, token.TextToken{"</b>"}
+	}
+	return nil, nil
+}
+
+func TestLexer(*testing.T) {
+	l := lexer.Must(lexer.New(
+		&TestMatcher{"bold", []lexer.Expression{
+			{Expr: `\[b\]`, CloseExpr: `\[/b\]`},
+		}},
+		&TestMatcher{"image", []lexer.Expression{
+			{Expr: `\[img=(.*?)\]`},
+			{Expr: `\[img]`, CloseExpr: `\[/img\]`, Flags: lexer.BodyAsArg},
+			{Expr: `\[img=(.*?)\]`, CloseExpr: `\[/img\]`, Flags: lexer.BodyAsArg},
+		}},
+		&TestMatcher{"noparse", []lexer.Expression{
+			{Expr: `\[nope\]`, CloseExpr: `\[/nope\]`, Flags: lexer.NoParseInner},
+		}},
+	))
+
+	toTokenize := `[b][img=http://image.com/cool.png][/b][b]hi
+[nope][img]http://fail.com/fun.png[/img][/nope]great.
+`
+
+	for _, t := range l.Tokenize(toTokenize) {
+		fmt.Print(t.(token.TextToken).Body)
 	}
 	fmt.Println()
 
 	// OUTPUT:
-	// <span class="at-tag user-0"data-uid="0" data-user="alice">alice</span> and <span class="at-tag user-1"data-uid="1" data-user="bob">bob</span> say that @charlie sucks because he doesn't use {{insert awesome service here}}
+	// <b><img src="http://image.com/cool.png" title="http://image.com/cool.png"></b><b>hi
+	// [img]http://fail.com/fun.png[/img]great.
+	// </b>
 }
